8cdc162699b99b4cd8edbbd4245788e43931b427
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-29 18:44:46 -0500

    Create & update Ownable.sol

---
+++ Ownable.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+contract Ownable {
+  address public owner;
+
+  constructor() {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner, "now owner");
+    _;
+  }
+
+  function setOwner(address _newOwner) external onlyOwner {
+    require(_newOwner != address(0), "invalid address");
+
+    owner = _newOwner;
+  }
+  function onlyOwnerCanCallThisFunc() external onlyOwner {
+    // Code.
+  }
+  function anyOneCanCall() external {
+    // Code.
+  }
+}
