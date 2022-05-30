4a07f0ff01a4978f3f3faa329e71abadb422604e
Ángel Sánchez <mektigboy@protonmail.com>
2022-05-30 13:48:29 -0500

    Update Ownable.sol

--- Ownable.sol
+++ Ownable.sol
@@ -2,26 +2,26 @@
 pragma solidity ^0.8.14;
 
 contract Ownable {
-  address public owner;
+    address public owner;
 
-  constructor() {
-    owner = msg.sender;
-  }
+    constructor() {
+        owner = msg.sender;
+    }
 
-  modifier onlyOwner() {
-    require(msg.sender == owner, "now owner");
-    _;
-  }
+    modifier onlyOwner() {
+        require(msg.sender == owner, "now owner");
+        _;
+    }
 
-  function setOwner(address _newOwner) external onlyOwner {
-    require(_newOwner != address(0), "invalid address");
+    function setOwner(address _newOwner) external onlyOwner {
+        require(_newOwner != address(0), "invalid address");
 
-    owner = _newOwner;
-  }
-  function onlyOwnerCanCallThisFunc() external onlyOwner {
-    // Code.
-  }
-  function anyOneCanCall() external {
-    // Code.
-  }
+        owner = _newOwner;
+    }
+    function onlyOwnerCanCallThisFunc() external onlyOwner {
+        // Code.
+    }
+    function anyOneCanCall() external {
+        // Code.
+    }
 }
